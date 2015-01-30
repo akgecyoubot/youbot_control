ddf72f35800090fc391b4d4435beb2fa633e8c6f
youbot <youbot@KUKA-youBot.(none)>
2015-01-30 12:07:48 +0300

    migrating to catkin

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/youbot/GitHub/pybotserver/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/youbot/GitHub/pybotserver/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/youbot/GitHub/pybotserver/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/youbot/GitHub/pybotserver/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/youbot/GitHub/pybotserver/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/youbot/GitHub/pybotserver/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/youbot/GitHub/pybotserver/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/youbot/GitHub/pybotserver/build"
+export PYTHONPATH="/home/youbot/GitHub/pybotserver/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/youbot/GitHub/pybotserver/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/youbot/GitHub/pybotserver/src:/opt/ros/groovy/share:/opt/ros/groovy/stacks"
