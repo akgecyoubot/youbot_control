e2620942acce6e11132df273a04d6c6422bcec8b
Kirill Morozov <kirillmorozov@users.noreply.github.com>
2015-01-31 18:32:19 +0300

    cleaning up

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/youbot/GitHub/pybotserver/src
-# Build directory: /home/youbot/GitHub/pybotserver/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
