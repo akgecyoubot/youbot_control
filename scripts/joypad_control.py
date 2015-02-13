b3ac35f3817f791870369bd594dfbc29a41c5101
Kirill Morozov <kirillmorozov@users.noreply.github.com>
2015-02-13 11:39:20 +0300

    gripper control and joint 1 movement

--- scripts/joypad_control.py
+++ scripts/joypad_control.py
@@ -5,7 +5,7 @@ u"""Node контролирующий youBot с джойстика."""
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
-from brics_actuator.msg import JointVelocities, JointValue
+from brics_actuator.msg import JointValue, JointPositions, JointVelocities
 
 
 class JoypadControlNode(object):
@@ -15,11 +15,12 @@ class JoypadControlNode(object):
     def __init__(self):
         u"""Функция инициализации."""
         self.base_velocity = Twist()
+        self.gripper_position = JointPositions()
         self.arm_velocities = JointVelocities()
-        self.base_velocity_publisher = rospy.Publisher('cmd_vel', Twist)
-        self.arm_velocities_publisher = rospy.Publisher('velocity_command',
-                                                        JointVelocities)
-        rospy.Subscriber("joy", Joy, self.update_data_from_joy)
+        self.base_velocity_publisher = rospy.Publisher('/cmd_vel', Twist)
+        self.gripper_position_publisher = rospy.Publisher('/arm_1/gripper_controller/position_command', JointPositions)
+        self.arm_velocities_publisher = rospy.Publisher('/arm_1/arm_controller/velocity_command', JointVelocities)
+        rospy.Subscriber("/joy", Joy, self.update_data_from_joy)
 
     def update_data_from_joy(self, data):
         u""" Обновляет скорость при поступлении данных с джойстика.
@@ -40,19 +41,71 @@ class JoypadControlNode(object):
         self.base_velocity.linear.y = data.axes[0] / 3.0
         self.base_velocity.angular.z = data.axes[4] / 3.0
         # Arm velocities handling
-        a = JointValue()
-        self.arm_velocities.velocities.append(a)
-        self.arm_velocities.velocities[0].joint_uri = 'arm_joint_1'
-        # self.arm_velocities.velocities.unit = 's^-1 rad'
-        self.arm_velocities.velocities[0].value = data.axes[2] / 3.0
+        self.gripper_position.positions = []
+        # Open gripper
+        if data.buttons[6]:
+            A = JointValue()
+            A.joint_uri = 'gripper_finger_joint_r'
+            A.value = 0.011499
+            A.unit = 'm'
+            A.timeStamp = rospy.Time.now()
+            self.gripper_position.positions.append(A)
+            B = JointValue()
+            B.joint_uri = 'gripper_finger_joint_l'
+            B.value = 0.011499
+            B.unit = 'm'
+            B.timeStamp = rospy.Time.now()
+            self.gripper_position.positions.append(B)
+        # Close gripper
+        if data.buttons[7]:
+            A = JointValue()
+            A.joint_uri = 'gripper_finger_joint_r'
+            A.value = 0
+            A.unit = 'm'
+            A.timeStamp = rospy.Time.now()
+            self.gripper_position.positions.append(A)
+            B = JointValue()
+            B.joint_uri = 'gripper_finger_joint_l'
+            B.value = 0
+            B.unit = 'm'
+            B.timeStamp = rospy.Time.now()
+            self.gripper_position.positions.append(B)
+        # Joint 1 movement
+        self.arm_velocities.velocities = []
+        if notXOR(data.buttons[4], data.buttons[5]):
+            A = JointValue()
+            A.joint_uri = 'arm_joint_1'
+            A.unit = 's^-1 rad'
+            A.value = 0
+            self.arm_velocities.velocities.append(A)
+        elif data.buttons[4]:
+            A = JointValue()
+            A.joint_uri = 'arm_joint_1'
+            A.unit = 's^-1 rad'
+            A.value = 0.5
+            self.arm_velocities.velocities.append(A)
+        elif data.buttons[5]:
+            A = JointValue()
+            A.joint_uri = 'arm_joint_1'
+            A.unit = 's^-1 rad'
+            A.value = -0.5
+            self.arm_velocities.velocities.append(A)
 
     def run(self):
         u"""Запускает Node и публикует сообщения по топику cmd_vel."""
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             self.base_velocity_publisher.publish(self.base_velocity)
-            self.arm_velocities_publisher.publish(self.arm_velocities)
+            if self.gripper_position.positions:
+                self.gripper_position_publisher.publish(self.gripper_position)
+            if self.arm_velocities.velocities:
+                self.arm_velocities_publisher.publish(self.arm_velocities)
             rate.sleep()
+
+
+def notXOR(A, B):
+    """Return True if only one of the inputs is True."""
+    return (not A and not B) or (A and B)
 
 
 if __name__ == '__main__':
