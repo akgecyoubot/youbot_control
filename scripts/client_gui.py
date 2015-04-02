86b4a572e39abb92c7d0a56648e906da0ea77815
Kirill Morozov <kirillmorozov@users.noreply.github.com>
2015-04-02 17:45:50 +0300

    Moved global variables from if __name__ == '__main__'

--- scripts/client_gui.py
+++ scripts/client_gui.py
@@ -7,6 +7,12 @@ import ttk
 import rospyoubot
 
 
+BASE_VELOCITY = 1
+ARM_VELOCITY = 1
+R1 = rospyoubot.YouBot()
+ARM_JOINTS_ANGLES = [tk.StringVar() for i in range(5)]
+ODOMETRY = [tk.StringVar() for i in range(3)]
+
 class MainApplication(ttk.Frame):
 
     u"""Основное окно приложения."""
@@ -440,15 +446,10 @@ def update_joints_labels():
 
 
 if __name__ == '__main__':
-    BASE_VELOCITY = 1
-    ARM_VELOCITY = 1
-    R1 = rospyoubot.YouBot()
     ROOT = tk.Tk()
     ROOT.title("youBot control")
     ROOT.resizable(1, 0)
     ROOT.columnconfigure(0, weight=1)
-    ARM_JOINTS_ANGLES = [tk.StringVar() for i in range(5)]
-    ODOMETRY = [tk.StringVar() for i in range(3)]
     MAINFRAME = MainApplication(ROOT)
     ROOT.update()
     ROOT.minsize(ROOT.winfo_width(), ROOT.winfo_height())
