27a6becc7124a6c7e9fee2c19820a9c007891777
Kirill Morozov <kirillmorozov@users.noreply.github.com>
2015-04-30 14:37:08 +0300

    unittest for Base class

--- scripts/test_rospyoubot.py
+++ scripts/test_rospyoubot.py
@@ -1,70 +1,60 @@
 #!/usr/bin/python2
 # coding=UTF-8
 """This module is for testing rospyoubot module."""
+PKG = 'pybotserver'
 
 import rospyoubot
-from math import radians, sin, cos
+from math import radians
+import unittest
 
-class TestBase:
+class BaseTestCase(unittest.TestCase):
     def test_calculate_velocity(self):
-        assert rospyoubot.calculate_velocity(0, 0) == (0, 0)
-        assert rospyoubot.calculate_velocity(5, 0) == (1, 0)
-        assert rospyoubot.calculate_velocity(0, 5) == (0, 1)
-        assert rospyoubot.calculate_velocity(5, 5) == (1, 1)
-        assert rospyoubot.calculate_velocity(-5, 0) == (-1, 0)
-        assert rospyoubot.calculate_velocity(0, -5) == (0, -1)
-        assert rospyoubot.calculate_velocity(-5, -5) == (-1, -1)
-        assert rospyoubot.calculate_velocity(0.1, 0) == (0.1, 0)
-        assert rospyoubot.calculate_velocity(0, 0.1) == (0, 0.1)
-        assert rospyoubot.calculate_velocity(0.1, 0.1) == (0.1, 0.1)
+        func = rospyoubot.calculate_velocity
+        cases = {(0, 0): (0, 0),
+                 (5, 0): (1, 0),
+                 (0, 5): (0, 1),
+                 (5, 5): (1, 1),
+                 (-5, 0): (-1, 0),
+                 (0, -5): (0, -1),
+                 (-5, -5): (-1, -1),
+                 (0.1, 0): (0.1, 0),
+                 (0, 0.1): (0, 0.1),
+                 (0.1, 0.1): (0.1, 0.1)}
+        for test in cases.keys():
+            self.assertEqual(func(*test),
+                             cases[test],
+                             'Wrong velocity calculated.')
 
     def test_transform_coordinates(self):
-        assert rospyoubot.transform_coordinates(0, 0, 0, 0, 0) == (0, 0)
-        assert rospyoubot.transform_coordinates(1, 0, 0, 0, 0) == (1, 0)
-        assert rospyoubot.transform_coordinates(0, 1, 0, 0, 0) == (0, 1)
-        assert rospyoubot.transform_coordinates(1, 1, 0, 0, 0) == (1, 1)
-        assert rospyoubot.transform_coordinates(-1, 0, 0, 0, 0) == (-1, 0)
-        assert rospyoubot.transform_coordinates(0, -1, 0, 0, 0) == (0, -1)
-        assert rospyoubot.transform_coordinates(-1, -1, 0, 0, 0) == (-1, -1)
-        assert rospyoubot.transform_coordinates(0, 0, 1, 0, 0) == (-1, 0)
-        assert rospyoubot.transform_coordinates(0, 0, 0, 1, 0) == (0, -1)
-        assert rospyoubot.transform_coordinates(0, 0, 1, 1, 0) == (-1, -1)
-        assert rospyoubot.transform_coordinates(0, 0, 0, 0, radians(45)) == (0, 0)
-        # assert rospyoubot.transform_coordinates(1, 1, 0, 0, radians(45)) == (1 * sin(radians(45)), 1 * cos(radians(45)))
-
-def tst_robot():
-    import rospy
-    """Test rospyoubot functionality."""
-    robot = rospyoubot.YouBot()
-    timer = rospy.Rate(0.25)
-    timer.sleep()
-    print "Testing base velocities command..."
-    robot.base.set_velocity(0.1, 0.1, -0.1)
-    timer.sleep()
-    robot.base.set_velocity()
-    timer.sleep()
-    print "Testing gripper position command..."
-    robot.arm.gripper.set_gripper_state(False)
-    timer.sleep()
-    robot.arm.gripper.set_gripper_state(True)
-    timer.sleep()
-    print "Testing arm position command..."
-    robot.arm.set_joints_angles(2.95, 1.1, -2.6, 1.8, 2.95)
-    timer.sleep()
-    robot.arm.set_joints_angles(0.0100693,
-                                0.0100693,
-                                -0.015708,
-                                0.0221239,
-                                0.11062)
-    timer.sleep()
-    print "Testing arm velocities commad..."
-    robot.arm.set_joints_velocities(0.1, 0.1, -0.1, 0.1, 0.1)
-    timer.sleep()
-    robot.arm.set_joints_velocities(0, 0, 0, 0, 0)
-    timer.sleep()
-    print "Going home..."
-    robot.arm.set_joints_angles(0.0100693,
-                                0.0100693,
-                                -0.015708,
-                                0.0221239,
-                                0.11062)
+        func = rospyoubot.transform_coordinates
+        cases = {(0, 0, 0, 0, 0): (0, 0),
+                 (1, 0, 0, 0, 0): (1, 0),
+                 (0, 1, 0, 0, 0): (0, 1),
+                 (1, 1, 0, 0, 0): (1, 1),
+                 (-1, 0, 0, 0, 0): (-1, 0),
+                 (0, -1, 0, 0, 0): (0, -1),
+                 (-1, -1, 0, 0, 0): (-1, -1),
+                 (0, 0, 1, 0, 0): (-1, 0),
+                 (0, 0, 0, 1, 0): (0, -1),
+                 (0, 0, 1, 1, 0): (-1, -1)}
+        almostEqualCases = {(0, 0, 0, 0, radians(45)): (0, 0),
+                            (5, 5, 0, 0, radians(30)): (6.83, 1.83),
+                            (5, 5, 0, 0, radians(70)): (6.41, -2.99),
+                            (5, 5, 0, 0, radians(190)): (-5.79, -4.06),
+                            (5, 5, 0, 0, radians(245)): (-6.64, 2.42),
+                            (5, 5, 3, 2, radians(35)): (1.31, -3.359),
+                            (5, 5, 3, 2, radians(120)): (-3.232, -1.598),
+                            (5, 5, 3, 2, radians(190)): (-2.135, 2.905),
+                            (5, 5, 3, 2, radians(310)): (3.46, 1.013)}
+        for test in cases.keys():
+            self.assertEqual(func(*test),
+                             cases[test])
+        for test in almostEqualCases.keys():
+            real = func(*test)
+            expected = almostEqualCases[test]
+            self.assertAlmostEqual(real[0],
+                                   expected[0],
+                                   places=2)
+            self.assertAlmostEqual(real[1],
+                                   expected[1],
+                                   places=2)
